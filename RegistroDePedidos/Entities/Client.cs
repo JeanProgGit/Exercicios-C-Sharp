2567df6c3a7f30ffd23eb42a8983f45364f28bf2
JeanProgGit <54381415+JeanProgGit@users.noreply.github.com>
2021-06-25 13:42:11 -0300

    Registro de pedidos

---
+++ RegistroDePedidos/Entities/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AppS9A123ExProp.Entities {
+    class Client {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+
+        public Client() {
+
+        }
+
+        public Client(string name, string email, DateTime date) {
+            Name = name;
+            Email = email;
+            Date = date;
+        }
+
+        public override string ToString() {
+            return Name
+                + ", ("
+                + Date.ToString("dd/MM/yyyy")
+                + ") - "
+                + Email;
+        }
+    }
+}
+
+
+
---
+++ RegistroDePedidos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using AppS9A123ExProp.Entities.Enums;
+using AppS9A123ExProp.Entities;
+
+namespace AppS9A123ExProp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre com os dados do cliente:");
+            Console.Write("Nome: ");
+            string clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Data de aniversário (DD/MM/YYYY): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Console.Write("Entre com os dados do pedido: ");
+            Console.Write("Status (PendingPayment/Processing/Shipped/Delivered): ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(clientName, email, date);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("Quantos itens para esse pedido? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine($"#{i}Entre com os dados do item");
+                Console.Write("Nome do produto: ");
+                string name = Console.ReadLine();
+                Console.Write("Preço do produto: ");
+                double price = double.Parse(Console.ReadLine());
+
+                Product product = new Product(name, price);
+
+                Console.Write("Quantidade: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                OrderItem OrderItem = new OrderItem(quantity, price, product);
+
+                order.AddItem(OrderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Sumário do pedido");
+            Console.WriteLine(order);
+
+
+        }
+    }
+}
